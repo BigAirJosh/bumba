01d272dbc32041328a9442eccbc38633f54db780
BigAirJosh <BigAirJosh@users.noreply.github.com>
2022-07-31 16:05:12 +0100

    add enemy placement and turn placeholder

--- tile_types.py
+++ tile_types.py
@@ -40,12 +40,12 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 floor = new_tile(
         walkable=True,
         transparent=True,
-        dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
-        light=(ord(" "), (255, 255, 255), (200, 180, 50)),
+        dark=(ord(" "), (255, 255, 255), (38, 43, 68)),
+        light=(ord(" "), (255, 255, 255), (58, 68, 102)),
         )
 wall = new_tile(
         walkable=False,
         transparent=False,
-        dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
-        light=(ord(" "), (255, 255, 255), (130, 110, 50)),
+        dark=(ord(" "), (255, 255, 255), (24, 20, 37)),
+        light=(ord(" "), (255, 255, 255), (38, 43, 68)),
         )
